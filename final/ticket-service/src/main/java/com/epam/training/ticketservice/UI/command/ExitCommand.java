a3d33b0bfae64675219e279045265042ec5e344a
Kiss_Gergely <kissgeri01@gmail.com>
2023-11-26 22:02:58 +0100

    Exit hozzaadasa

---
+++ final/ticket-service/src/main/java/com/epam/training/ticketservice/UI/command/ExitCommand.java
@@ -0,0 +1,14 @@
+package com.epam.training.ticketservice.UI.command;
+
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.commands.Quit;
+
+@ShellComponent
+public class ExitCommand implements Quit.Command{
+
+    @ShellMethod(key = "exit", value = "terminates the program")
+    public void terminate(){
+        System.exit(0);
+    }
+}
